791da79442c8984b453eae8888c66fcfc6d50b8f
root <admin@vpntech.net>
2018-06-09 23:25:50 -0400

    fix typo

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-module.export = {
+module.exports = {
     url: 'http://www.vpngate.net/api/iphone/',
     cacheSeconds: 60 * 30,
     vpnConcurrency: 15,
     vpnTimeLimit: 5000,
-    openvpn = '/usr/sbin/openvpn',
+    openvpn: '/usr/sbin/openvpn',
 };
