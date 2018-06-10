6a472ca2bfb36adf7094caeedf22fa3cdb30d52b
root <admin@vpntech.net>
2018-06-09 23:21:57 -0400

    parser logic config

---
+++ config.js
@@ -0,0 +1,7 @@
+module.export = {
+    url: 'http://www.vpngate.net/api/iphone/',
+    cacheSeconds: 60 * 30,
+    vpnConcurrency: 15,
+    vpnTimeLimit: 5000,
+    openvpn = '/usr/sbin/openvpn',
+};
