d70c944a7ce1c97bffd99f8646702e2775358060
rick <rick@ricks-MacBook-Air.local>
2018-06-09 23:44:23 -0400

    reference environment object

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
+var l = console.log;
 module.exports = {
-    url: process.VPNLISTURL || 'http://www.vpngate.net/api/iphone/',
-    cacheSeconds: process.CACHESECONDS || 60 * 30,
-    vpnConcurrency: process.VPNCONCURRENCY || 15,
-    vpnTimeLimit: process.VPNTIMELIMIT || 5000,
-    openvpn: process.OPENVPN || '/usr/sbin/openvpn',
-    vpnConfigFileDirectory: process.VPNCONFIGFILEDIRECTORY || __dirname+'/vpnConfigFiles',
+    url: process.env.VPNLISTURL || 'http://www.vpngate.net/api/iphone/',
+    cacheSeconds: process.env.CACHESECONDS || 60 * 30,
+    vpnConcurrency: process.env.VPNCONCURRENCY || 15,
+    vpnTimeLimit: process.env.VPNTIMELIMIT || 5000,
+    openvpn: process.env.OPENVPN || '/usr/sbin/openvpn',
+    vpnConfigFileDirectory: process.env.VPNCONFIGFILEDIRECTORY || __dirname+'/vpnConfigFiles',
 };
