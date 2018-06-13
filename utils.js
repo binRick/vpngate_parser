aab1d818698551366d1723a22123cd06b2675f8a
root <admin@vpntech.net>
2018-06-13 00:57:03 -0400

    utils script

---
+++ utils.js
@@ -0,0 +1,18 @@
+module.exports = {
+countryList: (obj) => {
+    const countries = [];
+    for (const i in obj) {
+        countries.push(obj[i].CountryShort);
+    }
+
+    function unique(arr) {
+        const obj = {};
+        for (const i in countries) {
+            const str = arr[i];
+            obj[str] = true;
+        }
+        return Object.keys(obj);
+    }
+    return unique(countries);
+},
+};
