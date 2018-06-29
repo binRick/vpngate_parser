e9d94e038ea10e480142da155f8e19d109ae9025
root <admin@vpntech.net>
2018-06-28 22:08:15 -0400

    centos reqs

--- centos7.sh
+++ centos7.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-yum -y install redis hiredis bc python2-pyyaml && \
+yum -y install redis hiredis bc python2-pyyaml tmux && \
 	rm -rf tmuxp && \
 	git clone https://github.com/tmux-python/tmuxp && cd tmuxp && python setup.py install && \
 	systemctl start redis && \
