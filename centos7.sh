10e99eec8ae4f1ccca01952ede01e5ca67b8906e
root <admin@vpntech.net>
2018-06-28 20:48:10 -0400

    add centos yums

--- centos7.sh
+++ centos7.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 command bc >/dev/null 2>&1 || sudo yum -y install bc
+yum -y install redis hiredis 
+systemctl start redis
+systemctl enable redis
 export VPNCONCURRENCY=50
 export VPNTIMELIMIT=5000
 export MAXVPNS=15
-./vpngate_parser.js
+echo ./vpngate_parser.js
