33813332b0641d0451a6bed158bf290e28dc76a0
root <admin@vpntech.net>
2018-06-28 20:54:44 -0400

    set debug env var option

--- centos7.sh
+++ centos7.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 command bc >/dev/null 2>&1 || sudo yum -y install bc
-yum -y install redis hiredis 
-systemctl start redis
-systemctl enable redis
+yum -y install redis hiredis  >/dev/null 2>&1
+systemctl start redis >/dev/null 2>&1
+systemctl enable redis >/dev/null 2>&1
 export VPNCONCURRENCY=50
 export VPNTIMELIMIT=5000
 export MAXVPNS=15
