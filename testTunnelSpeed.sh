cd4004a566cd186201359d09bb07e048388eca61
root <admin@vpntech.net>
2018-06-28 20:26:32 -0400

    add sysctl sets

--- testTunnelSpeed.sh
+++ testTunnelSpeed.sh
@@ -10,8 +10,8 @@ TUN_ID_HEX=$(echo "obase=16; $TUN_ID" | bc| tr "[:upper:]" "[:lower:]")
 SPEED_TEST_URL="https://github.com:${TUN_PORT}/jamesward/play-load-tests/raw/master/public/10mb.txt"
 SPEED_TEST_URL="https://raw.githubusercontent.com:${TUN_PORT}/smclt30p/speedtest/master/5MB"
 
-sysctl -w net.ipv4.conf.default.rp_filter=2
-sysctl -w net.ipv4.conf.all.rp_filter=2
+sysctl -w net.ipv4.conf.default.rp_filter=2 >/dev/null 2>&1
+sysctl -w net.ipv4.conf.all.rp_filter=2 >/dev/null 2>&1
 
 ls $RX_B_FILE >/dev/null || { echo "Tunnel \"$TUN\" not up." && exit 1; }
 ip route | grep $TUN >/dev/null && ip route add 0.0.0.0/0 dev $TUN table $TUN_ID 2>/dev/null
