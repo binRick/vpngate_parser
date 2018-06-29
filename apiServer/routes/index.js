0e2bc7c0490914961f5b52d094edf919cac7a55d
root <admin@vpntech.net>
2018-06-28 22:25:47 -0400

    ip location feature

--- apiServer/routes/index.js
+++ apiServer/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     request = require('request'),
     router = express.Router(),
+    async = require('async'),
     _ = require('underscore'),
     l = console.log,
     ip2location = require('ip-to-location');
@@ -24,17 +25,20 @@ router.get('/', function(req, res, next) {
         request.get(urls.completes.replace('__QTY__', stats.complete.qty), function(e, dat) {
             if (e) throw e;
             stats.complete.data = JSON.parse(dat.body);
-            stats.complete.data = stats.complete.data.map(function(o) {
+            async.map(stats.complete.data, function(o, _cb) {
                 ip2location.fetch(o.data.IP, function(err, res) {
                     if (err) throw err;
                     o.ipInfo = res;
+                    _cb(null, o);
+                });
+            }, function(er, data) {
+                stats.complete.data = data;
+                l(stats.complete.data[0]);
+                res.render('index', {
+                    stats: stats
                 });
             });
 
-            l(stats.complete.data[0]);
-            res.render('index', {
-                stats: stats
-            });
         });
     });
 });
