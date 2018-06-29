7d9fbf95bc451e2342d6cc9f82253479d877e229
root <admin@vpntech.net>
2018-06-28 22:03:27 -0400

    centos reqs

--- centos7.sh
+++ centos7.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
-command bc >/dev/null 2>&1 || sudo yum -y install bc
-yum -y install redis hiredis  >/dev/null 2>&1
-systemctl start redis >/dev/null 2>&1
-systemctl enable redis >/dev/null 2>&1
-export VPNCONCURRENCY=50
-export VPNTIMELIMIT=5000
-export MAXVPNS=15
+yum -y install redis hiredis bc python2-pyyaml
+systemctl start redis
+systemctl enable redis
 echo ./vpngate_parser.js
