62b33a2dd3185d0e14957f8e1f50dd463fa68762
root <admin@vpntech.net>
2018-06-28 22:04:54 -0400

    centos reqs

--- centos7.sh
+++ centos7.sh
@@ -3,4 +3,5 @@ yum -y install redis hiredis bc python2-pyyaml && \
 	pip install tmuxp && \
 	systemctl start redis && \
 	systemctl enable redis && \
-	echo ./vpngate_parser.js
+	echo ./vpngate_parser.js && \
+	echo "tmuxp load kueRedisServices-tmuxp.yaml"
