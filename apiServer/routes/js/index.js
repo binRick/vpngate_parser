1c4aaf5302d3901323f18d2d5c81239327f11253
root <admin@vpntech.net>
2018-06-28 22:37:51 -0400

    datamaps

--- apiServer/routes/js/index.js
+++ apiServer/routes/js/index.js
@@ -3,4 +3,36 @@ var l = console.log;
 $(document).ready(function(){
 	l('ready to go');
 	$('#vpnEndpoints').dataTable();
+
+
+    var map = new Datamap({
+        element: document.getElementById('vpn-map'),
+        fills: {
+            HIGH: '#afafaf',
+            LOW: '#123456',
+            MEDIUM: 'blue',
+            UNKNOWN: 'rgb(0,0,0)',
+            defaultFill: 'green'
+        },
+        data: {
+            IRL: {
+                fillKey: 'LOW',
+                numberOfThings: 2002
+            },
+            USA: {
+                fillKey: 'MEDIUM',
+                numberOfThings: 10381
+            }
+        },
+        geographyConfig: {
+            popupTemplate: function(geo, data) {
+                return ['<div class="hoverinfo"><strong>',
+                        'Number of things in ' + geo.properties.name,
+                        ': ' + data.numberOfThings,
+                        '</strong></div>'].join('');
+            }
+        }
+    });
+
+
 });
