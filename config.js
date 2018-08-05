972e34b49a81334c96ae8816861de9d7949d3630
root <root@vpn4.vpnservice.company>
2018-08-05 01:35:21 -0400

    lower concurrency

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var l = console.log;
 module.exports = {
     url: process.env.VPNLISTURL || 'http://www.vpngate.net/api/iphone/',
     cacheSeconds: process.env.CACHESECONDS || 60 * 30,
-    vpnConcurrency: process.env.VPNCONCURRENCY || 200,
+    vpnConcurrency: process.env.VPNCONCURRENCY || 25,
     vpnTimeLimit: process.env.VPNTIMELIMIT || 5000,
     openvpn: process.env.OPENVPN || '/usr/sbin/openvpn',
     vpnConfigFileDirectory: process.env.VPNCONFIGFILEDIRECTORY || __dirname + '/vpnConfigFiles',
